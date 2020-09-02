118e40a60f63df1bae433a3c23597a999d841382
marzione00 <marzio.decorato@outlook.it>
2020-09-02 16:35:33 +0200

    pub

--- R_code_unsup.R
+++ R_code_unsup.R
@@ -13,6 +13,7 @@ library(mdendro)
 Dati_Covid <- data.frame(read_excel("C:/Users/Marzio/Desktop/Progetti ML/Dati Covid_prov_FULL.xlsx"))
 row.names(Dati_Covid)<- Dati_Covid[,2]
 prov_full.pca<-prcomp(Dati_Covid[,5:11], center = TRUE,scale. = TRUE)
+summary(prov_full.pca)
 autoplot(prov_full.pca,loadings = TRUE,loadings.size= 4, loadings.colour = 'blue',
          loadings.label = TRUE, loadings.label.size = 4,label=TRUE,label.vjust=1.5,loadings.label.repel=T,label.repel=T)+ggtitle('Totale') + theme_bw() + theme(plot.title = element_text(hjust = 0.5))
 fviz_eig(prov_full.pca,barcolor = "red",barfill = "red",geom = c("bar"),addlabels= TRUE )+labs(title = "Variances - PCA",x = "Principal Components", y = "% of variances")+theme_bw()+theme(plot.title = element_text(hjust = 0.5))
@@ -39,13 +40,13 @@ heatmap(x = cor(Dati_Covid[,5:11]), col = palette, symm = TRUE, margins = c(10, 
 # Dati Regioni Full
 Dati_Covid_reg <- data.frame(read_excel("C:/Users/Marzio/Desktop/Progetti ML/Dati Covid reg_FULL.xlsx"))
 row.names(Dati_Covid_reg) <-Dati_Covid_reg[,1]
-reg_full.pca<-prcomp(Dati_Covid_reg[,2:13], center = TRUE,scale. = TRUE)
+reg_full.pca<-prcomp(Dati_Covid_reg[,2:12], center = TRUE,scale. = TRUE)
 autoplot(reg_full.pca,loadings = TRUE,loadings.size= 4, loadings.colour = 'blue',
          loadings.label = TRUE, loadings.label.size = 4,label=FALSE,label.vjust=1.5,loadings.label.repel=T,label.repel=T) +ggtitle('Dati regionali - Totale') +theme_bw()+theme(plot.title = element_text(hjust = 0.5))
 summary(reg_full.pca)
 fviz_eig(reg_full.pca,barcolor = "red",barfill = "red",geom = c("bar"),addlabels= TRUE )+labs(title = "Variances - PCA",x = "Principal Components", y = "% of variances")+theme_bw()+theme(plot.title = element_text(hjust = 0.5))
 fviz_pca_var(reg_full.pca,col.var = "contrib",gradient.cols = c("red","orange","blue"),repel = TRUE,col.circle = "black",arrowsize = 1,labelsize = 0.5,jitter = list(what = "both", width = 1, height = 1) ) +theme_bw()+theme(plot.title = element_text(hjust = 0.5))
-corrplot(cor(Dati_Covid_reg[,2:13]))
+corrplot(cor(Dati_Covid_reg[,2:12]))
 palette = colorRampPalette(c("green", "blue", "red")) (20)
 heatmap(x = cor(Dati_Covid_reg[,2:12]), col = palette, symm = TRUE, margins = c(10, 10),main = 'Regioni Tot', dist(Dati_Covid_reg[,2:12],method = 'euclidean'))
 dend.dat<-dist(Dati_Covid_reg[,2:4] )
