c769fd58493a06e9f0012eba89f20e2e544762b7
marzione00 <marzio.decorato@outlook.it>
2020-09-03 14:26:49 +0200

    pub

--- R_code_unsup.R
+++ R_code_unsup.R
@@ -21,7 +21,7 @@ fviz_pca_var(prov_full.pca,col.var = "contrib",gradient.cols = c("red","orange",
 
 corrplot(cor(Dati_Covid[,5:11]))
 palette = colorRampPalette(c("green", "blue", "red")) (20)
-heatmap(x = cor(Dati_Covid[,5:11]), col = palette, symm = TRUE, margins = c(10, 10), main = 'Province TOT',dist(Dati_Covid[,5:11],method = 'euclidean'))
+heatmap(x = cor(Dati_Covid[,5:11]), col = palette, symm = TRUE, margins = c(10, 10), main = 'Provinces',dist(Dati_Covid[,5:11],method = 'euclidean'))
 
 # Dati Province nel mese di Agosto 
 Dati_Covid_prov_AUG <- data.frame(read_excel("C:/Users/Marzio/Desktop/Progetti ML/Dati Covid_prov_AUG.xlsx"))
@@ -40,16 +40,16 @@ heatmap(x = cor(Dati_Covid[,5:11]), col = palette, symm = TRUE, margins = c(10, 
 # Dati Regioni Full
 Dati_Covid_reg <- data.frame(read_excel("C:/Users/Marzio/Desktop/Progetti ML/Dati Covid reg_FULL.xlsx"))
 row.names(Dati_Covid_reg) <-Dati_Covid_reg[,1]
-reg_full.pca<-prcomp(Dati_Covid_reg[,2:12], center = TRUE,scale. = TRUE)
+reg_full.pca<-prcomp(Dati_Covid_reg[,2:11], center = TRUE,scale. = TRUE)
 autoplot(reg_full.pca,loadings = TRUE,loadings.size= 4, loadings.colour = 'blue',
-         loadings.label = TRUE, loadings.label.size = 4,label=FALSE,label.vjust=1.5,loadings.label.repel=T,label.repel=T) +ggtitle('Dati regionali - Totale') +theme_bw()+theme(plot.title = element_text(hjust = 0.5))
+         loadings.label = TRUE, loadings.label.size = 4,label=FALSE,label.vjust=1.5,loadings.label.repel=T,label.repel=T) +ggtitle('Regions') +theme_bw()+theme(plot.title = element_text(hjust = 0.5))
 summary(reg_full.pca)
 fviz_eig(reg_full.pca,barcolor = "red",barfill = "red",geom = c("bar"),addlabels= TRUE )+labs(title = "Variances - PCA",x = "Principal Components", y = "% of variances")+theme_bw()+theme(plot.title = element_text(hjust = 0.5))
 fviz_pca_var(reg_full.pca,col.var = "contrib",gradient.cols = c("red","orange","blue"),repel = TRUE,col.circle = "black",arrowsize = 1,labelsize = 0.5,jitter = list(what = "both", width = 1, height = 1) ) +theme_bw()+theme(plot.title = element_text(hjust = 0.5))
-corrplot(cor(Dati_Covid_reg[,2:12]))
+corrplot(cor(Dati_Covid_reg[,2:11]))
 palette = colorRampPalette(c("green", "blue", "red")) (20)
-heatmap(x = cor(Dati_Covid_reg[,2:12]), col = palette, symm = TRUE, margins = c(10, 10),main = 'Regioni Tot', dist(Dati_Covid_reg[,2:12],method = 'euclidean'))
-dend.dat<-dist(Dati_Covid_reg[,2:4] )
+heatmap(x = cor(Dati_Covid_reg[,2:12]), col = palette, symm = TRUE, margins = c(10, 10),main = 'Regions', dist(Dati_Covid_reg[,2:12],method = 'euclidean'))
+dend.dat<-dist(Dati_Covid_reg[,2:11] )
 ciccio<-as.ggdend(as.dendrogram(hclust(dend.dat,method= "average")))
 ggplot(ciccio,horiz = TRUE)
 fviz_dend(hclust(dend.dat,method= "average"),k = 10,palette="npg", repel=TRUE,color_labels_by_k = TRUE,cex = 0.5) +theme(plot.title = element_text(hjust = 0.5)) 
