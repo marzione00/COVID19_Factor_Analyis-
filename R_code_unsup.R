a090bca590595e620cc36d5ce817a43a5704ac9f
marzione00 <marzio.decorato@outlook.it>
2020-09-07 19:15:14 +0200

    pub

--- R_code_unsup.R
+++ R_code_unsup.R
@@ -48,7 +48,7 @@ fviz_eig(reg_full.pca,barcolor = "red",barfill = "red",geom = c("bar"),addlabels
 fviz_pca_var(reg_full.pca,col.var = "contrib",gradient.cols = c("red","orange","blue"),repel = TRUE,col.circle = "black",arrowsize = 1,labelsize = 0.5,jitter = list(what = "both", width = 1, height = 1) ) +theme_bw()+theme(plot.title = element_text(hjust = 0.5))
 corrplot(cor(Dati_Covid_reg[,2:11]))
 palette = colorRampPalette(c("green", "blue", "red")) (20)
-heatmap(x = cor(Dati_Covid_reg[,2:12]), col = palette, symm = TRUE, margins = c(10, 10),main = 'Regions', dist(Dati_Covid_reg[,2:12],method = 'euclidean'))
+heatmap(x = cor(Dati_Covid_reg[,2:11]), col = palette, symm = TRUE, margins = c(10, 10),main = 'Regions', dist(Dati_Covid_reg[,2:11],method = 'euclidean'))
 dend.dat<-dist(Dati_Covid_reg[,2:11] )
 ciccio<-as.ggdend(as.dendrogram(hclust(dend.dat,method= "average")))
 ggplot(ciccio,horiz = TRUE)
@@ -75,16 +75,16 @@ heatmap(x = cor(Dati_Covid_reg_AUG[,2:11]), col = palette, symm = TRUE, margins 
 
 Covid_mond <- data.frame(read_excel("C:/Users/Marzio/Desktop/Progetti ML/Covid_mond.xlsx"))
 row.names(Covid_mond) <-Covid_mond[,1]
-world.pca<-prcomp(Covid_mond[,6:15], center = TRUE,scale. = TRUE)
+world.pca<-prcomp(Covid_mond[,6:14], center = TRUE,scale. = TRUE)
 fviz_pca_var(world.pca,col.var = "contrib",gradient.cols = c("red","orange","blue"),repel = TRUE,col.circle = "black",arrowsize = 1,labelsize = 0.5,jitter = list(what = "both", width = 1, height = 1) ) +theme_bw()+theme(plot.title = element_text(hjust = 0.5)) 
 fviz_eig(world.pca,barcolor = "red",barfill = "red",geom = c("bar"),addlabels= TRUE )+labs(title = "Variances - PCA",x = "Principal Components", y = "% of variances")+theme_bw()+theme(plot.title = element_text(hjust = 0.5))
 
 autoplot(peppo4,loadings = TRUE,loadings.size= 4, loadings.colour = 'blue',
-         loadings.label = TRUE, loadings.label.size = 5,label=FALSE,label.vjust=1.5,loadings.label.repel=T,label.repel=T)+ggtitle('Dati Mondiali') +theme_bw()+theme(plot.title = element_text(hjust = 0.5)) 
+         loadings.label = TRUE, loadings.label.size = 5,label=FALSE,label.vjust=1.5,loadings.label.repel=T,label.repel=T)+ggtitle('World') +theme_bw()+theme(plot.title = element_text(hjust = 0.5)) 
 summary(world.pca)
-corrplot(cor(Covid_mond[,6:15]))
+corrplot(cor(Covid_mond[,6:14]))
 palette = colorRampPalette(c("green", "blue", "red")) (20)
-heatmap(x = cor(Covid_mond[,6:15]), col = palette, symm = TRUE, margins = c(10, 10),main = 'Mondo',dist(Covid_mond[,6:15],method = 'euclidean'))
+heatmap(x = cor(Covid_mond[,6:14]), col = palette, symm = TRUE, margins = c(10, 10),main = 'World',dist(Covid_mond[,6:14],method = 'euclidean'))
 
 
 
